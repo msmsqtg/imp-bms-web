014df5d20ba758dcc38ab22950fe4a640a604f78
quanintech <xiejn@sqqmall.com>
2025-08-12 16:09:56 +0800

    优化

--- src/views/report-management/components/walking/valueDetails.vue
+++ src/views/report-management/components/walking/valueDetails.vue
@@ -32,8 +32,8 @@
           {{row.type == "1" ? '团长' : '团员'}}
         </template>
       </el-table-column> 
-      <el-table-column prop="leaderName" label="团长姓名"></el-table-column>
-      <el-table-column prop="leaderPhone" label="团长手机号"></el-table-column>  
+      <el-table-column prop="leaderName" label="关联团长姓名"></el-table-column>
+      <el-table-column prop="leaderPhone" label="关联团长手机"></el-table-column>  
       <el-table-column fixed="right" label="操作" width="100">
         <template  #default="{ row }">
           <el-button @click="handleClick(row)" type="text" >活力值记录</el-button>
